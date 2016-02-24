c3349c18b244d7a7bbb4e1b8aebd495a9fd07518
Palmer Harrell <palmerharrell@me.com>
2016-02-24 09:55:21 -0600

    Added Composition examples

--- scripts/manufactory.js
+++ scripts/manufactory.js
@@ -1,39 +1,78 @@
 "use strict";
 
-function Car() {
+function Vehicle() {
 	this.manufactured_date = Date.now();
+	this.model = null;
+	this.tires = [];
 }
+
+// Car is a Vehicle
+function Car() {}
+Car.prototype = new Vehicle(); // Establish prototype chain
+
+// Truck is a Vehicle
+function Truck() {}
+Truck.prototype = new Vehicle(); 
+
+// Van is a Vehicle
+function Van() {}
+Van.prototype = new Vehicle(); 
 
 // Adding a method to the Car prototype:
 Car.prototype.changeManufacturedDate = function(newDate) {
 	this.manufactured_date = newDate;
 }
 
+// Car *has* a Make
 function Make(maker) {
 	this.manufacturer = maker;
 }
-Make.prototype = new Car();
 
+// Honda is a Make
 function Honda(name) {
 	this.modelName = name;
 }
-Honda.prototype = new Make("Honda");
+Honda.prototype = new Make("Honda"); 
 
+// Accord is a Honda
 function Accord() {
 }
-Accord.prototype = new Honda("Accord");
+Accord.prototype = new Honda("Accord"); 
 
-var accord = new Accord();
+// Car *has* Tires
+function Tire() {}
 
-/*
-    Being an avid car collector, you own three different cars all of
-    same model from your favorite manufacturer.
+// Pirelli is a Tire
+function Pirelli() {}
+Pirelli.prototype = new Tire();
 
-    Create three new objects for each of your three favorite cars.
-*/
-var accord2 = new Accord();
-var accord3 = new Accord();
-var accord4 = new Accord();
+// PExtreme is a Pirelli
+function PExtreme() {}
+PExtreme.prototype = new Pirelli();
+
+
+
+
+var aNewCar = new Car(); // Create an instance of Car
+var myAccord = new Accord(); // Create an instance of Accord
+
+var Tire1 = new PExtreme();
+var Tire2 = new PExtreme();
+var Tire3 = new PExtreme();
+var Tire4 = new PExtreme();
+
+// Composition
+aNewCar.model = myAccord;
+aNewCar.tires.push(Tire1, Tire2, Tire3, Tire4);
+
+console.log("aNewCar: ", aNewCar);
+
+
+// Create three new objects for each of your three favorite cars.
+
+var myAccord2 = new Accord();
+var myAccord3 = new Accord();
+var myAccord4 = new Accord();
 
 /*
     Being a rich, avid car collector, you own 2 cars from your second
